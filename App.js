c03f6099c71edc3fd0e8d15d8a5dfc8b12e9ef6f
Gabriel Ribeiro <gabriel.ribeiro@tecnospeed.com.br>
2025-01-14 10:29:52 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen'; // Importa a tela de login
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoginScreen /> {/* Chama a tela de login */}
       <StatusBar style="auto" />
     </View>
   );
