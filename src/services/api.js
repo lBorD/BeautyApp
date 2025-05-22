9a50d686cc4bc6c7bbe6a409909140d8b5f3a018
Gabriel RIbeiro <gabriel.ribeiro@tecnospeed.com.br>
2025-05-22 16:18:13 -0300

    change timeout

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://api-h1hk.onrender.com',
-  baseURL: 'https://c77d-2804-f84-5-1156-6c13-c40f-9581-bf79.ngrok-free.app',
-  timeout: 5000,
+  baseURL: 'https://api-h1hk.onrender.com',
+  //baseURL: 'https://c77d-2804-f84-5-1156-6c13-c40f-9581-bf79.ngrok-free.app',
+  timeout: 40000,
   headers: {
     'Content-Type': 'application/json',
     'ngrok-skip-browser-warning': 'true',
