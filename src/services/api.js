09e3706f6b25a5cf3ca827145e206db3c24254d7
Gabriel RIbeiro <gabriel.ribeiro@tecnospeed.com.br>
2025-05-22 16:08:38 -0300

    expo sdk upgrated

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'https://api-h1hk.onrender.com',
-  baseURL: 'https://4fd9-2804-f84-5-1156-992f-1b50-4bca-f928.ngrok-free.app',
+  baseURL: 'https://c77d-2804-f84-5-1156-6c13-c40f-9581-bf79.ngrok-free.app',
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
